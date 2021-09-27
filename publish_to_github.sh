4c669af14d89477a32559d7f9b9df50c3807634c
Michael Varamashvili <mv@fx.gl>
2021-09-27 11:16:11 +0300

    Update to v0.3.7

--- publish_to_github.sh
+++ publish_to_github.sh
@@ -7,8 +7,8 @@ git checkout master .
 
 git commit -m "Update to v${ROBUSTA_VERSION}"
 
-git tag "v${ROBUSTA_VERSION}"
+git tag -f "v${ROBUSTA_VERSION}"
 
-git push --tags github-prod github-prod-master:master
+git push -f --tags github-prod github-prod-master:master
 
-git push --tags origin github-prod-master
+git push -f --tags origin github-prod-master
